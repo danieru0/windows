a2e55f3bd9409ed9c036518273d37a09df1d7b30
Daniel Dąbrowski <elosik123@onet.pl>
2019-05-01 11:46:51 +0200

    Updates

---
+++ precache-manifest.480cc08790010cb1ddc92e4526ad5b4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1cb3cfdc920cc859a306",
+    "url": "/elosiktv/windows/static/js/runtime~main.190991a9.js"
+  },
+  {
+    "revision": "dc6459eecd7622c9f9c3",
+    "url": "/elosiktv/windows/static/js/main.4384102a.chunk.js"
+  },
+  {
+    "revision": "312edfc5289d64ce2f15",
+    "url": "/elosiktv/windows/static/js/2.a45ac386.chunk.js"
+  },
+  {
+    "revision": "dc6459eecd7622c9f9c3",
+    "url": "/elosiktv/windows/static/css/main.9e813c7a.chunk.css"
+  },
+  {
+    "revision": "05326697a40b992b57097024dd77eca2",
+    "url": "/elosiktv/windows/index.html"
+  }
+];
